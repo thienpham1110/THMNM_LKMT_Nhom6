95f5d3cf9b82e5a20a81900889431da9718a1f21
thienpham1110 <thienthien111099@gmail.com>
2020-12-28 09:19:30 +0700

    kiem tra va sua loi tren nhanh truoc khi merge

--- LKMT/DAO/DBContext.Designer.cs
+++ LKMT/DAO/DBContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\OSS\THMNM_LKMT_Nhom6\LKMT\DAO\DBContext.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\gitth\THMNM_LKMT_Nhom6\LKMT\DAO\DBContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
